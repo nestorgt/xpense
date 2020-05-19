042495d0665a717dd67bfae5399c3bc3eb3a4cae
Nestor Garcia <nestorgtrabazo@gmail.com>
2020-05-19 15:18:34 +0200

    Initial Commit

---
+++ Xpense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xpense
+//
+//  Created by Nestor Garcia on 19/05/2020.
+//  Copyright © 2020 nestor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
